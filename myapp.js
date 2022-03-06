ce932bef0b534203917680fab3649770d8d9e631
Priya Savaliya <pr930067@dal.ca>
2022-03-06 03:01:36 -0400

    begin request final

--- myapp.js
+++ myapp.js
@@ -4,9 +4,9 @@ var bodyParser = require('body-parser')
 
 const AWS=require('aws-sdk');
 const { json } = require('body-parser');
-const KEY_ID= "ASIA2XWSATAKFMYKLDXS"; 
-const SECRET_KEY="4vsnoC8OZ9WJH+wk4aDjCXIbslNeir3JNFRqCSVn";
-const TOKEN="FwoGZXIvYXdzEA4aDPsixpfUAzatXvZQXiLAAXP6zwSjee4ey4uQrswX0HQ3VdsVDbckRBU99JYDzfc4d5XUnLUcIVCnwqblGfcr5RRVMjBXrzCAK2zA0nXdmBgLUKRSTy6GciNdciYfniUjjXzwqx79y2w1TwGPVvVPjgCDzxz7VFRAl4WL1QzZAwU7+/rLCYbp86G8q/ra02BM+CzKi7wW16aMAWIv2yn8XkkpPwvsLvv6yAVAvXwGbusaWJdnvlGbTavfGEwThjpeeWr3WN/Y5Z4KR80BOqaFfijU8YmRBjIt95Znzn+HKlz3Z+cLypeBCS7Abg3qV2GzdE/t7Zci41d2S3KUilST83xkRUye";
+const KEY_ID= "ASIA2XWSATAKA2Z7LSFE"; 
+const SECRET_KEY="JOL6OIc2VTEycABZsYF5NHpIqAdSsR2NC+jQAD15";
+const TOKEN="FwoGZXIvYXdzEDAaDCnVOJoejc1lvKx/nyLAAROsTr8KIdto38IKznenHbGHdJzX4rYPMDrLw/fpRzzjG/fb2kEn/b+a+wrQJ57nCrNoAvzrleMg0iaaJePMaZ5oY8hfl41zpbiEb8CDIJYy/TLregCIS7S3BwwQlSj5it/wPSY3fWyVeCwNM7N7XM8QCZHfncdI2lqSklwzo63O5l5uZo5v/SxkrwO0p/tIUwamvUM7fvqT+Vchd34Fz9lvJWiDWNrHsNCXGWg1459wJ8YiV/zUq9wnADrx0F55fijospGRBjItK90z7IF4IcIZpd9GzuZtI4DLw9w6UumD1GaN3W4vcqQX4zUaI1tu7yF0y5J1";
 const s3= new AWS.S3({  
     accessKeyId:KEY_ID,
     secretAccessKey: SECRET_KEY,
